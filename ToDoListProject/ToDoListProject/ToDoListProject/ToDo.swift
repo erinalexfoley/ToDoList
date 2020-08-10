f1641304a96b6c896ca59b1d916adac5261eada6
erinalexfoley <69254577+erinalexfoley@users.noreply.github.com>
2020-08-10 17:21:02 -0400

    Add files via upload
    Completed Iteration 0

---
+++ ToDoListProject/ToDoListProject/ToDoListProject/ToDo.swift
@@ -0,0 +1,14 @@
+//
+//  ToDo.swift
+//  ToDoListProject
+//
+//  Created by Erin Foley on 8/10/20.
+//  Copyright © 2020 Erin Foley. All rights reserved.
+//
+
+import UIKit
+
+class ToDoClass {
+    var description = ""
+    var important = false
+}
---
+++ ToDoListProject/ToDoListProject/ToDoListProject/ToDoTableViewController.swift
@@ -0,0 +1,71 @@
+//
+//  ToDoTableViewController.swift
+//  ToDoListProject
+//
+//  Created by Erin Foley on 8/10/20.
+//  Copyright © 2020 Erin Foley. All rights reserved.
+//
+
+import UIKit
+
+class ToDoTableViewController: UITableViewController {
+
+    
+    var listOfToDo : [ToDoClass] = []
+    
+    func createToDo() -> [ToDoClass] {
+        
+        let swiftToDo = ToDoClass()
+        swiftToDo.description = "Learn Swift"
+        swiftToDo.important = true
+        
+        let dogToDo = ToDoClass()
+        dogToDo.description = "Walk the Dog"
+        
+        return [swiftToDo, dogToDo]
+        
+    }
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        listOfToDo = createToDo()
+    }
+
+
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        let eachToDo = listOfToDo[indexPath.row]
+        cell.textLabel?.text = eachToDo.description
+        
+        if eachToDo.important {
+            cell.textLabel?.text = "❗️" + eachToDo.description
+        } else {
+            cell.textLabel?.text = eachToDo.description
+        }
+
+        return cell
+    }
+   
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+ */
+
+}
